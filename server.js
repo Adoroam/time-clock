1af6e64b3152e6a82d6fcc635511e973fef75603
Anthony <astabile.design@gmail.com>
2017-02-28 13:13:21 -0600

    committing minor changes before latop overheats

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 80
 
 app.use(express.static(__dirname))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+/* DB STUFF */
+mongoose.connect('mongodb://localhost/timeclock')// connect to db
+const db = mongoose.connection// set variable
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {})
+const itemSchema = mongoose.Schema({
+  user: String,
+  desc: String,
+  completed: Boolean,
+  time: { start: Date.now, end: false, total: false }
+})
+const Item = mongoose.model('Item', itemSchema)
 
 app.get('/', (req, res) => {
+  res.redirect('/')
+})
+
+app.post('/clock', (req, res) => {
+  let newItem = new Item
   res.redirect('/')
 })
 
