af3d916574fa51e1bd7b65ef0e4ce0465bed39b7
Anthony Stabile <astabile.design@gmail.com>
2017-03-03 03:42:37 -0600

    added ngcookies, can now register new users, logging in makes a cookie, the cookie is read in angular and shows if you're logged in

--- js/ang.js
+++ js/ang.js
@@ -1,6 +1,23 @@
-const app = angular.module('app', [])
+const app = angular.module('app', ['ngCookies'])
 
-app.controller('indCtrl', ['$scope', function ($scope) {
+app.factory('getUsers', function ($http) {
+  return $http.post('/users')
+})
+app.controller('indCtrl', ['$scope', '$cookies', 'getUsers', function ($scope, $cookies, getUsers) {
   const ind = this
-  // empty controller
+  if ($cookies.get('user')) {
+    let cookieSplit = $cookies.get('user').split(':')
+    ind.cookieId = cookieSplit[1]
+  }
+  getUsers.then(d => {
+    let data = d.data
+    ind.userObj = data.find(item => item._id === ind.cookieId)
+  })
+}])
+
+app.controller('loginCtrl', ['$scope', 'getUsers', function ($scope, getUsers) {
+  const log = this
+  getUsers.then(d => {
+    log.data = d.data
+  })
 }])
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const port = 80
 
 app.use(express.static(__dirname))
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -13,22 +15,82 @@ mongoose.connect('mongodb://localhost/timeclock')// connect to db
 const db = mongoose.connection// set variable
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {})
-const itemSchema = mongoose.Schema({
+// schemas
+
+const userSchema = mongoose.Schema({
+  fullName: String,
   user: String,
-  desc: String,
-  completed: Boolean,
-  time: { start: Date.now, end: false, total: false }
+  pass: String,
+  signup: Date
 })
-const Item = mongoose.model('Item', itemSchema)
+const User = mongoose.model('User', userSchema)
 
 app.get('/', (req, res) => {
   res.redirect('/')
 })
 
-app.post('/clock', (req, res) => {
-  let newItem = new Item
+app.post('/register', (req, res) => {
+  let newName = req.body.rname
+  let newUser = req.body.ruser
+  let newPass = req.body.rpass
+  let hash = bastion(newPass)
+  let register = new User({
+    fullName: newName,
+    user: newUser,
+    pass: hash,
+    signup: new Date()
+  })
+  register.save((err, registeredUser) => {
+    if (err) console.error(err)
+    console.log(`new user: ${registeredUser}`)
+  })
   res.redirect('/')
 })
+
+app.post('/users', (req, res) => {
+  User.find((err, list) => {
+    if (err) console.error(err)
+    res.json(list)
+  })
+})
+
+app.post('/login', (req, res) => {
+  let user = req.body.lname
+  let pass = req.body.lpass
+  let hash = bastion(pass)
+  let query = User.findOne({'user': user})
+  query.select('user pass')
+  query.exec((err, foundUser) => {
+    if (err) console.error(err)
+    if (foundUser.pass === hash) {
+      let cookieVal = `${foundUser.user}:${foundUser._id}`
+      res.cookie('user', cookieVal)
+      res.redirect('/')
+    } else {
+      res.redirect('/')
+    }
+  })
+})
+
+// app.get('/clear', (req, res) => {
+//   User.remove({}, () => {
+//     console.log('users removed')
+//   })
+// })
+
+function bastion (pass) {
+  let pwarray = pass.split('')
+  let pwlen = pwarray.length
+  let modifier = pwlen < 5 ? 5 : pwlen
+  let iteration1 = pwarray.map(item => {
+    let charCode = item.charCodeAt(0)
+    let newCode = String.fromCharCode(charCode + modifier)
+    return newCode
+  })
+  let iteration2 = iteration1.reverse()
+  let iteration3 = iteration2.join('')
+  return iteration3
+}
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`)
